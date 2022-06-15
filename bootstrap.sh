a9fb6ea79e2d236d39780bb45b90290e854d3d9a
Takuya TAKAHASHI <38676393+takutakahashi@users.noreply.github.com>
2022-06-15 18:48:24 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cp -f $HOME/.dotfiles/home/.zshrc $HOME/.zshrc
-cp -rf $HOME/.dotfiles/home/.config $HOME/.config
 curl -sSfL \
   https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.0.0/aqua-installer |
   bash
+aqua -c $HOME/.dotfiles/home/.config/aquaproj-aqua/aqua.yaml i
