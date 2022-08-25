c88bf75c1335e7ab567ab59a583da793deb12b9a
Takuya TAKAHASHI <taku.takahashi120@gmail.com>
2022-08-26 00:24:53 +0900

    add packer.nvim

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
 cp -f $HOME/.dotfiles/home/.zshrc $HOME/.zshrc
 cp -rf $HOME/.dotfiles/home/.config/nvim $HOME/.config/
+git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
 curl -sSfL \
   https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.0.0/aqua-installer |
   bash
