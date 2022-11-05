3f7100118ac280758ae56f220247edaca2f78ebf
Takuya TAKAHASHI <38676393+takutakahashi@users.noreply.github.com>
2022-11-05 15:56:48 +0900

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ cp -rf $HOME/.dotfiles/home/.config/nvim $HOME/.config/
 sudo cp -rf $HOME/.dotfiles/home/.dev/bin/* /usr/local/bin/
 git clone https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/opt/packer.nvim
 curl -sSfL \
-  https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.19.2/aqua-installer |
+  https://raw.githubusercontent.com/aquaproj/aqua-installer/v1.0.0/aqua-installer |
   bash
 export AQUA_GLOBAL_CONFIG=${AQUA_GLOBAL_CONFIG:-}:${XDG_CONFIG_HOME:-$HOME/.config}/aquaproj-aqua/aqua.yaml
 aqua i -a
