5f90e6579fe6fb6bcff963331e1defa0da6c745d
高橋 拓也 <takutaka@pepabo.com>
2023-08-24 14:52:09 +0900

    gopls config

---
+++ home/.config/nvim/lua/user/lsp/config/gopls.lua
@@ -0,0 +1,11 @@
+return {
+  on_attach = function ()
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "<buffer>",
+      callback = function()
+        vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+      end
+    })
+    -- vim.cmd "autocmd BufWritePre <buffer> lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })"
+  end
+}
