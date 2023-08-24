60859928ff1aa6cab00027fcfe6eeadcce7cc714
高橋 拓也 <takutaka@pepabo.com>
2023-08-24 15:46:45 +0900

    mappings

---
+++ home/.config/nvim/lua/user/mappings.lua
@@ -0,0 +1,13 @@
+return {
+  n = {
+    ["<S-h>"] = { 'hhhhh' },
+    ["<S-j>"] = { 'jjjjj' },
+    ["<S-k>"] = { 'kkkkk', remap = true },
+    ["<S-l>"] = { 'lllll' },
+    fh = { '<Leader>bp', remap = true },
+    --fj = { '', remap = true },
+    fk = { '<C-k>', remap = true },
+    fl = { ':bnext<CR>', remap = true },
+    fq = { ':bd<CR>' }
+  }
+}
